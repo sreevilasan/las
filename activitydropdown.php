b710e2a2bfe9c8e7aa14bd4ae75fe8e70b980998
sreevilasan <sreevilasan@gmail.com>
2017-09-06 08:46:14 +0530

    Changed dropdown

--- activitydropdown.php
+++ activitydropdown.php
@@ -2,9 +2,7 @@
 	// File: ActivityDropdown.php
 	// 
 
-//	require 'include/sessioncheck.php';
 	require 'include/commonclass.php';
-//	require 'include/Header.php';
 	
 	$table1 = 'deptact';
 	$table2 = 'activity';
@@ -14,7 +12,7 @@
 	
 	$db = new Database();	// open database
 
-	$sql = "select " . $table1 . ".actId as actNo, activity from " . $table1 . ", " . $table2 . " where " . $table1 . ".deptId=" . $deptId . " and " . $table1 . ".actId=" . $table2 . ".actId ;"; 
+	$sql = "select " . $table1 . ".actId as actNo, activity from " . $table1 . ", " . $table2 . " where " . $table1 . ".deptId=" . $deptId . " and " . $table1 . ".actId=" . $table2 . ".actId order by activity;"; 
 
 	$rows = $db->select($sql);
 	
@@ -24,28 +22,10 @@
 		// exit();
 	}
 	foreach ($rows as $row) {
+		// key="p-d-a"; if (key in keyArray) skip next statement
 		echo '<option value="' . $row['actNo'] . '">' . $row['activity'] . '</option>';
 	}
 			
 	$db->close(); 	// close database connection
-
-/*
-echo '<option disabled selected></option>';
-echo '<option value="1">Sree2</option>';
-echo '<option value="2">Report</option>';
-echo '<option value="3">Visit</option>';
-echo '<option value="4">Calculation</option>';
-echo '<option value="5">BOQ</option>';
-echo '<option value="6">Drawing</option>';
-echo '<option value="7">Isometric</option>';
-echo '<option value="8">Layout</option>';
-echo '<option value="9">Planning</option>';
-echo '<option value="10">Approval</option>';
-echo '<option value="11">Review</option>';
-echo '<option value="12">Support</option>';
-echo '<option value="13">Specification</option>';
-echo '<option value="14">RCC</option>';
-echo '<option value="15">Structure</option>';
-*/
 
 ?>
